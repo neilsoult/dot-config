8f1887ede356394b2031338bb270d68ebb261c26
Neil Soult <neil.soult@gmail.com>
2022-12-04 13:57:54 +0700

    exa is not stable

--- pi/.zsh/aliases.zsh
+++ pi/.zsh/aliases.zsh
@@ -3,6 +3,6 @@
 alias a="ansible"
 alias ap="ansible-playbook"
 # ALIAS COMMANDS
-alias ls="exa --icons --group-directories-first"
-alias ll="exa --icons --group-directories-first -l"
+#alias ls="exa --icons --group-directories-first"
+#alias ll="exa --icons --group-directories-first -l"
 alias grep="grep --color"
