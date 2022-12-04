df27c36f377ee164ca31acaa80fb20a77b9899a8
Neil Soult <neil.soult@gmail.com>
2022-12-04 12:57:04 +0700

    reorg, create separate configuration folders for separate purposes

---
+++ Pi/.zsh/aliases.zsh
@@ -0,0 +1,8 @@
+# Alias
+# ---
+alias a="ansible"
+alias ap="ansible-playbook"
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias grep="grep --color"
