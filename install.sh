978cf19613041be54ac6319d4d6788dd15f31c4d
Neil Soult <neil.soult@gmail.com>
2024-10-29 11:37:31 +0100

    debugging

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ cp ./.gitconfig ~
 # Oh My Zsh!
 echo "download oh my zsh"
 echo dirname "$0"
-sh -c "$(wget -O- https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh)"
-sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
+sh -c "$(wget -qO https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh)"
+sudo sh -c "$(wget -qO https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
 echo "copy configs"
 cp ./.zshrc ~
 cp ./zshenv ~
 cp -r ./.zsh ~
 # Nerd Fonts
 echo "download and install nerdfonts"
-mkdir -p /usr/share/fonts && cd /usr/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
-unzip RobotoMono.zip
-rm RobotoMono.zip && cd ~
+sudo mkdir -p /usr/share/fonts && cd /usr/share/fonts
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
+sudo unzip RobotoMono.zip
+sudo rm RobotoMono.zip && cd ~
 # Starship
 echo "download starship"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y -f
