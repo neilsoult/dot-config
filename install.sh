2caa41274a96d9ce43a7fcf1f9e9ea4f94c92918
Neil Soult <neil.soult@gmail.com>
2024-10-29 11:26:55 +0100

    remove sudo from install script

--- install.sh
+++ install.sh
@@ -9,17 +9,17 @@ cp ./.gitconfig ~
 echo "download oh my zsh"
 echo dirname "$0"
 sh -c "$(wget -O- https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh)"
-sudo sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/deluan/zsh-in-docker/master/zsh-in-docker.sh)"
 echo "copy configs"
 cp ./.zshrc ~
 cp ./zshenv ~
 cp -r ./.zsh ~
 # Nerd Fonts
 echo "download and install nerdfonts"
-sudo mkdir -p /usr/share/fonts && cd /usr/share/fonts
-sudo wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
-sudo unzip RobotoMono.zip
-sudo rm RobotoMono.zip && cd ~
+mkdir -p /usr/share/fonts && cd /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/RobotoMono.zip
+unzip RobotoMono.zip
+rm RobotoMono.zip && cd ~
 # Starship
 echo "download starship"
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y -f
